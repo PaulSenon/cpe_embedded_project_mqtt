59fda9c824446f98cd2fc3ef83baafdae0f314d8
PaulSenon <paul.senon@gmail.com>
2019-04-24 15:06:33 +0200

    test

--- rf_sensor/sensor.c
+++ rf_sensor/sensor.c
@@ -286,7 +286,7 @@ void releves(uint32_t gpio){
     // on update pas les relevé si on est en attente d'ACK
     if(waitForACK == 0){
         // Tous les relevés :
-            rlv_buffer[0] = releve_temp();
+            rlv_buffer[0] = 42;
             // rlv_buffer[1] = ...
 
         // send on rf
